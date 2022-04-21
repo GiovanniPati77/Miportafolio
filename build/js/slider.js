6cdb5cabdaa283a757dc80fb13aeceea509ecd05
GiovanniPati77 <85655940+GiovanniPati77@users.noreply.github.com>
2022-04-21 13:14:07 -0500

    first commit

---
+++ build/js/slider.js
@@ -0,0 +1,48 @@
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+let letra = document.getElementById("text");
+let str = letra.innerHTML;
+
+letra.innerHTML = "";
+
+let velocidad = 200;
+let i = 0;
+
+function escribir() {
+  if (i < str.length) {
+    letra.innerHTML += str.charAt(i);
+    i++;
+    setTimeout(escribir, velocidad);
+  }
+}
+setTimeout(escribir, velocidad);
